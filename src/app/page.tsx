c1460d97a45e63f6ff848bd943e6ec96a908ec1d
Greg Finley <gregory.finley@gmail.com>
2023-12-24 14:22:49 -0800

    Start rebuild mode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,6 +14,7 @@ interface CurrentState {
 export default function Home() {
   const [currentState, setCurrentState] = useState<CurrentState | null>(null);
   const [refreshIndex, setRefreshIndex] = useState(0);
+  const [rebuildMode, setRebuildMode] = useState(false);
 
   useEffect(() => {
     setCurrentState({
@@ -27,22 +28,29 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
       <div className="z-10 max-w-5xl w-full flex-col items-center font-mono text-sm lg:flex">
         <p className="mb-2 fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          😱 Billy&apos;s snowman just melted! He snapped a picture before it
-          happened. Take a gander and then help him rebuild it!
+          😱 Billy&apos;s snowman just melted!
+          <br />
+          He snapped a picture before it happened.
+          <br />
+          Take a gander and then help him rebuild it!
         </p>
         {currentState &&
-          Object.entries(currentState).map((item, index) => (
-            <Image
-              key={index}
-              src={toAssetFileName(item[0], item[1])}
-              alt=""
-              width={100}
-              height={24}
-              priority
-            />
+          (rebuildMode ? (
+            <p>Haha just kidding!</p>
+          ) : (
+            Object.entries(currentState).map((item, index) => (
+              <Image
+                key={index}
+                src={toAssetFileName(item[0], item[1])}
+                alt=""
+                width={100}
+                height={24}
+                priority
+              />
+            ))
           ))}
         <button
-          className="mt-3 bg-blue-300 hover:bg-blue-400 text-white font-bold py-2 px-4 rounded-full shadow-md hover:shadow-lg transition duration-300 ease-in-out transform hover:-translate-y-1"
+          className="mt-3 bg-blue-300 hover:bg-blue-400 text-white font-bold py-2 px-4 rounded-full shadow-md hover:shadow-lg transition duration-300 ease-in-out transform"
           onClick={() => setRefreshIndex(refreshIndex + 1)}
         >
           Refresh
