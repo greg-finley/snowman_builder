6d6ec5d159374411dfecb6230392bd1a684454af
Greg Finley <gregory.finley@gmail.com>
2023-12-24 13:41:29 -0800

    Fix up

--- src/app/utils.ts
+++ src/app/utils.ts
@@ -1,8 +1,6 @@
 import _ from "lodash";
 
-const baseRandom = (assetName: string) =>
-  _.sample([0, 1, 2, 3].map((i) => `/${assetName}${i}.png`)) as string;
+export const randomAssetIndex = () => _.sample([0, 1, 2, 3]) as number;
 
-export const randomHead = () => baseRandom("head");
-export const randomBody = () => baseRandom("body");
-export const randomHat = () => baseRandom("hat");
+export const toAssetFileName = (assetName: string, index: number) =>
+  `/${assetName}${index}.png`;
