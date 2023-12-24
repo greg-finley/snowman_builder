4fd4459999bb18ac691a8ef9e7a7fe698abea11f
Greg Finley <gregory.finley@gmail.com>
2023-12-24 12:24:07 -0800

    bodies and hats

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,12 @@
 import React, { useEffect, useState } from 'react';
 
 import Image from 'next/image'
-import {randomHead} from './util'
+import {randomHead, randomBody, randomHat} from './utils'
 
 export default function Home() {
-  const [head, setHead] = useState('');
-
-  useEffect(() => {
-    setHead(randomHead()); // Call the function when the component mounts
-  }, []); // The empty array ensures this effect runs once on mount
+  const [head, setHead] = useState(randomHead());
+  const [body, setBody] = useState(randomBody());
+  const [hat, setHat] = useState(randomHat());
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
@@ -18,35 +16,18 @@ export default function Home() {
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
         We are gonna build a snowman once we do some code
         </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            {head && <Image
-              src={head}
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />}
-          </a>
+        <div className="fixed bottom-0 left-0 flex-col h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+        {[hat, head, body].map((item, index) => (
+  <Image
+    key={index}
+    src={item}
+    alt=''
+    width={100}
+    height={24}
+    priority
+  />
+))}
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
       </div>
 
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
---
+++ src/app/utils.ts
@@ -0,0 +1,7 @@
+import _ from 'lodash';
+
+const baseRandom =(assetName: string) => _.sample( [0,1,2,3].map((i) => `/${assetName}${i}.png`)) as string;
+
+export const randomHead = () => baseRandom('head');
+export const randomBody = () => baseRandom('body');
+export const randomHat = () => baseRandom('hat');
