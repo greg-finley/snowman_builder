834c66a4337ee615b4b09f03c93717e590a9088f
Greg Finley <gregory.finley@gmail.com>
2023-12-24 10:04:53 -0800

    type assert

--- src/app/util.ts
+++ src/app/util.ts
@@ -1,4 +1,4 @@
 import _ from 'lodash';
 
-export const randomHead = () => _.sample( [0,1,2,3].map((i) => `/head${i}.png`));
+export const randomHead = () => _.sample( [0,1,2,3].map((i) => `/head${i}.png`)) as string;
     
