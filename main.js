ec8afcbce18179c5736cfbf4aaba283b234e2225
Greg Finley <gregory.finley@gmail.com>
2023-12-24 12:31:41 -0800

    Lint

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
-const path = require('path');
-const fs = require('fs');
+const path = require("path");
+const fs = require("fs");
 
-const eyesDir = path.join(__dirname, 'eyes');
+const eyesDir = path.join(__dirname, "eyes");
 const eyes = fs.readdirSync(eyesDir);
 
-const randomIntFromInterval = (min, max) => { // min and max included 
-     return Math.floor(Math.random() * (max - min + 1) + min)
-     }
+const randomIntFromInterval = (min, max) => {
+  // min and max included
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
 
 // Pick a random file from the four files in the directory
 const randomEyes = eyes[randomIntFromInterval(0, eyes.length - 1)];
-console.log('The random eyes are: ' + randomEyes);
+console.log("The random eyes are: " + randomEyes);
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-'use client';
+"use client";
 
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
-import Image from 'next/image'
-import {randomHead, randomBody, randomHat} from './utils'
+import Image from "next/image";
+import { randomHead, randomBody, randomHat } from "./utils";
 
 export default function Home() {
   const [head, setHead] = useState(randomHead());
@@ -14,19 +14,19 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-        We are gonna build a snowman once we do some code
+          We are gonna build a snowman once we do some code
         </p>
         <div className="fixed bottom-0 left-0 flex-col h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-        {[hat, head, body].map((item, index) => (
-  <Image
-    key={index}
-    src={item}
-    alt=''
-    width={100}
-    height={24}
-    priority
-  />
-))}
+          {[hat, head, body].map((item, index) => (
+            <Image
+              key={index}
+              src={item}
+              alt=""
+              width={100}
+              height={24}
+              priority
+            />
+          ))}
         </div>
       </div>
 
@@ -38,7 +38,7 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
+            Docs{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
@@ -55,7 +55,7 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
+            Learn{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
@@ -72,7 +72,7 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
+            Templates{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
@@ -89,7 +89,7 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
+            Deploy{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
@@ -100,5 +100,5 @@ export default function Home() {
         </a>
       </div>
     </main>
-  )
+  );
 }
