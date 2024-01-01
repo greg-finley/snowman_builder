733dd3203acfca5f803b1cd8cf352789a51fe7eb
Greg Finley <gregory.finley@gmail.com>
2023-12-31 17:13:22 -0800

    Two rows of two

--- src/app/components/SnowmanPartOptions.tsx
+++ src/app/components/SnowmanPartOptions.tsx
@@ -11,12 +11,27 @@ interface SnowmanPartOptionsProps {
 
 export default function SnowmanPartOptions(props: SnowmanPartOptionsProps) {
   const { snowmanPart, handleGuess } = props;
+  const shuffledIndexes = _.shuffle(indexes);
+  // Assumes we always have exactly 4 items
+  const firstRow = shuffledIndexes.slice(0, 2);
+  const secondRow = shuffledIndexes.slice(2);
 
   return (
-    <div>
-      <p>Which {snowmanPart} was it?</p>
-      <div className="flex flex-wrap gap-2">
-        {_.shuffle(indexes).map((index) => (
+    <div className="flex flex-col gap-2">
+      <p className="flex justify-center">Which {snowmanPart} was it?</p>
+      <div className="flex justify-center gap-2">
+        {firstRow.map((index) => (
+          <div
+            key={index + snowmanPart}
+            className="cursor-pointer"
+            onClick={() => handleGuess(snowmanPart, index)}
+          >
+            <SnowmanPartImage index={index} snowmanPart={snowmanPart} />
+          </div>
+        ))}
+      </div>
+      <div className="flex justify-center gap-2">
+        {secondRow.map((index) => (
           <div
             key={index + snowmanPart}
             className="cursor-pointer"
