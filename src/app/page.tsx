cf94c43dcfda88b2a3e1a385d634904f9543d174
Greg Finley <gregory.finley@gmail.com>
2023-12-31 17:03:27 -0800

    Improve styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -65,7 +65,7 @@ export default function Home() {
   };
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+    <main className="flex min-h-screen flex-col items-center justify-center">
       <div className="z-10 max-w-5xl w-full flex-col items-center font-mono text-sm lg:flex">
         <p className="mb-2 fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           😱 Billy&apos;s snowman just melted!
@@ -82,7 +82,7 @@ export default function Home() {
                 currentState={currentState}
                 label="Billy's snowman, before it melted"
               />
-              <div className="flex gap-4">
+              <div className="flex justify-center gap-4">
                 <Button
                   onClick={() => setGuessMode(snowmanParts[0])}
                   buttonText="Let's Rebuild!"
@@ -101,11 +101,13 @@ export default function Home() {
                 handleGuess={handleGuess}
                 snowmanPart={guessMode}
               />
-              <Button
-                onClick={handleBackToPhoto}
-                buttonText="Back to Photo"
-                variant="dull"
-              />
+              <div className="flex justify-center">
+                <Button
+                  onClick={handleBackToPhoto}
+                  buttonText="Back to Photo"
+                  variant="dull"
+                />
+              </div>
             </div>
           ) : (
             // Player is done guessing
